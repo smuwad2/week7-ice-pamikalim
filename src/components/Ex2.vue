cba9e47029d2aff56d731d806c570ce3c816fbc1
pamikalim <pamika.lim.2024@smu.edu.sg>
2025-10-22 02:11:43 +0000

    ex2

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -3,16 +3,58 @@
         // Add code in appropriate places
         data() {
             return {
-                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter']
+                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'],
+                newItem: ''   
             }
             
-        }
+        },
+    methods: {
+        addItem() {
+            if (this.newItem.trim() !== '') {
+                this.items.push(this.newItem.trim());
+                this.newItem = ''; 
+            }
+        },
+        deleteItem(index) {
+            this.items.splice(index, 1); 
     }
+}
+}
+    
 </script>
 
 <template>
 
     <h2>Shopping Cart</h2>
     <!-- TODO: Add Code Here -->
+     <input v-model="newItem" placeholder="Enter item name">
+    <button @click="addItem">Add!</button>
+
+    <ul>
+        <li v-for="(item, index) in items" :key="index">
+            {{ item }}
+            <button @click="deleteItem(index)">Delete</button>
+        </li>
+    </ul>
    
 </template>
+
+
+<style scoped>
+h2 {
+    font-family: Arial, sans-serif;
+}
+ul {
+    list-style: none;
+    padding: 0;
+}
+li {
+    margin: 5px 0;
+}
+input {
+    margin-right: 8px;
+}
+button {
+    margin-left: 8px;
+}
+</style>
